50357b424909cad2fb0cfd734a2dd10267abd6cb
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-07-21 11:47:48 -0700

    check-filter now handled by global bus

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -3,11 +3,11 @@
     <h2>Filter results</h2>
     <h3>By time of day</h3>
     <div class="filter-group">
-      <check-filter category="time" v-for="time in times" v-bind:title="time" v-on:check-filter="checkFilter" v-bind:key="time"></check-filter>
+      <check-filter category="time" v-for="time in times" v-bind:title="time" v-bind:key="time"></check-filter>
     </div>
     <h3>By genre</h3>
     <div class="filter-group">
-      <check-filter category="genre" v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter" v-bind:key="genre"></check-filter>
+      <check-filter category="genre" v-for="genre in genres" v-bind:title="genre" v-bind:key="genre"></check-filter>
     </div>
   </div>
 </template>
@@ -23,11 +23,6 @@ export default {
       times
     };
   },
-    methods: {
-      checkFilter(category, title, checked){
-        this.$emit('check-filter', category, title, checked);
-      }
-    },
     components: {
       CheckFilter
     }
